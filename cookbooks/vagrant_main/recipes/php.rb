fbe7efdd8dc8a68fce1d1170dbfce9389c42d725
Victor Grigoriev <vectoroc@gmail.com>
2013-03-02 17:24:08 +0400

    moved out xdebug settings from php-extra template

--- cookbooks/vagrant_main/recipes/php.rb
+++ cookbooks/vagrant_main/recipes/php.rb
@@ -31,3 +31,12 @@ template "#{node['php']['ext_conf_dir']}/php-extra.ini" do
   action :create
   notifies :restart, resources("service[apache2]"), :delayed
 end
+
+template "#{node['php']['ext_conf_dir']}/xdebug.ini" do
+  source "xdebug.ini.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  action :create
+  notifies :restart, resources("service[apache2]"), :delayed
+end
