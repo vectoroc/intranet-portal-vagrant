57149b8ced1a88cf95d4c7f733ec6aa071f287e0
Victor Grigoriev <v.grigoryev@i-free.com>
2014-03-10 11:18:57 +0400

    corrected mailcatcher upstart conf

--- cookbooks/vagrant_main/attributes/default.rb
+++ cookbooks/vagrant_main/attributes/default.rb
@@ -18,4 +18,5 @@ default['php']['xdebug']['trace_output_dir'] = '/tmp'
 
 default['mailcatcher']['http_ip'] = '0.0.0.0'
 default['mailcatcher']['http_port'] = '1080'
-default['mailcatcher']['smtp_port'] = '25'
+default['mailcatcher']['smtp_ip'] = '127.0.0.1'
+default['mailcatcher']['smtp_port'] = '1025'
